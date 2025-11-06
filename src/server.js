3ba269b3693f289f7236446c4aad0d64d1eed671
eternity961 <mikaelelias100@gmail.com>
2025-11-06 12:08:11 +0300

    first stage of payment integration

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const newsRoutes = require('./routes/news');
 const orderRoutes = require('./routes/orders');
 
 const app = express();
+
+const axios = require("axios").default
 app.use(cors());
 app.use(express.json());
 
@@ -18,8 +20,74 @@ connectDB().catch(err => {
   console.error('DB connection failed', err);
   process.exit(1);
 });
+const CHAPA_URL = process.env.CHAPA_URL || "https://api.chapa.co/v1/transaction/initialize"
+const CHAPA_AUTH = process.env.CHAPA_AUTH // || register to chapa and get the key
+const config = {
+    headers: {
+        Authorization: `Bearer ${CHAPA_AUTH}`
+    }
+}
+app.post("/api/pay", async (req, res) => {
+  const CALLBACK_URL = "http://localhost:5000/api/verify-payment/";
+  const RETURN_URL = "http://localhost:3000/payment-success?";
+  const TEXT_REF = "tx-myecommerce12345-" + Date.now();
+  const { amount, email, first_name, last_name } = req.body;
+
+  const data = {
+    amount,
+    currency: "ETB",
+    email,
+    first_name,
+    last_name,
+    tx_ref: TEXT_REF,
+    callback_url: CALLBACK_URL + TEXT_REF,
+    return_url: `${RETURN_URL}tx_ref=${TEXT_REF}`,
+  };
+
+  try {
+    const response = await axios.post(CHAPA_URL, data, config);
+    res.json({ checkout_url: response.data.data.checkout_url });
+  } catch (err) {
+    console.error("Payment init failed:", err.response?.data || err.message);
+    res.status(500).json({ error: "Payment initialization failed" });
+  }
+});
+
+
+// verification endpoint
+app.get('/api/payment/verify/:tx_ref', async (req, res) => {
+  try {
+    const { tx_ref } = req.params;
+    const verifyUrl = `https://api.chapa.co/v1/transaction/verify/${tx_ref}`;
+    const response = await axios.get(verifyUrl, {
+      headers: { Authorization: `Bearer ${process.env.CHAPA_AUTH}` },
+    });
+
+    res.json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Verification failed' });
+  }
+});
+
+
+app.get("/api/payment-success", async (req, res) => {
+    res.send("Payment was successful. You can now close this page")
+})
+
 
 // routes
+app.post("/api/webhook", express.json(), (req, res) => {
+  const event = req.body;
+  console.log("Webhook event:", event);
+  
+  if (event.status === "success") {
+    // Update your database: mark order as paid
+  }
+
+  res.status(200).send("OK");
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/news', newsRoutes);
